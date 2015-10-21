01166dabc6540fbe73d521113d7b037de4d887b0
unteroid <unteroid@gmail.com>
2015-10-21 22:33:09 +0300

    Garbage clean

--- RaspberryPi/Timer.cpp
+++ RaspberryPi/Timer.cpp
@@ -3,7 +3,6 @@
 #define PERIOD 2500000
 
 class_Timer Timer;
-//	FILE * myfile;
 
 class_Timer::class_Timer()
 {
@@ -11,7 +10,6 @@ class_Timer::class_Timer()
 	oldtime_.tv_nsec = 0;
 	time_.tv_sec = 0;
 	time_.tv_nsec = 0;
-    //timer_create(CLOCK_REALTIME, NULL, &timerId);
  }
 
 class_Timer::~class_Timer()
@@ -20,8 +18,6 @@ class_Timer::~class_Timer()
 
 void class_Timer::initialize() {
     clock_gettime(CLOCK_MONOTONIC, &time_);
-//	myfile = fopen("freq.txt","w");
-
 }
 
 
@@ -36,8 +32,4 @@ void class_Timer::get_dt()
 		clock_gettime(CLOCK_MONOTONIC, &time_);
     dt = ((static_cast<int64_t>(time_.tv_sec) * 1000000000 + static_cast<int64_t>(time_.tv_nsec)) - (static_cast<int64_t>(oldtime_.tv_sec) * 1000000000 + static_cast<int64_t>(oldtime_.tv_nsec))) / 1000000000.0;
 	}
-//			fprintf(myfile, "dt is %fhz \n",1/dt);
-
-//	if 
-
 }
--- RaspberryPi/Timer.h
+++ RaspberryPi/Timer.h
@@ -11,23 +11,12 @@ public:
     class_Timer();
     virtual ~class_Timer();
     
-    //void start();
-    //void stop();
-    
     float dt;
     void get_dt();
 	void initialize();
-    //timer_t timerId; //id  for the posix timer
-    //struct sigaction signalAction; //signal action handler struct
-    //bool started;
 private:
-    //static void sig_handler_(int signum);
-    //struct itimerspec timeToSet_; //time to be set
-    //struct timespec timeValue_; //timer expiration value
-    //struct timespec timeInterval_; //timer period
     timespec oldtime_;
     timespec time_;
-    //timespec iterationtime_;
     
 
 };
